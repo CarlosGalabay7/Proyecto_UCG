71c8354b4a18b2ba650bc8aba5493867c0854b9d
CarlosGalabay7 <carlos.galabay@casagrande.edu.ec>
2026-05-16 09:55:46 -0500

    Cargar archivos

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import pandas as pd
 import streamlit as st
 
 st.title("Proyecto Final Casa Grande")
 st.sidebar.title("Parámetros")
 st.sidebar.image("src/python_logo.png")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
